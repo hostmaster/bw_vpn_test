f0791154d7740023d840b05d0bec5226bac46962
Igor Khomyakov <github.site@4ck.me>
2022-08-02 10:49:46 +0300

    Update entrypoint-node.sh
    Fixing a silly bug

--- entrypoint-node.sh
+++ entrypoint-node.sh
@@ -6,7 +6,7 @@ CONF_FILE="/server_config/peer_${HOSTNAME}/peer_${HOSTNAME}.conf"
 if [ -f "${CONF_FILE}" ]; then
     cp -u "$CONF_FILE" /config/wg0.conf
 else
-    "Echo no config file found: $CONF_FILE"
+    echo "No config file found: $CONF_FILE"
 fi
 
 exec "$@"
